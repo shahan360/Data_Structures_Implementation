dbe890a670f535bebbf11487ebad504fc07c0a92
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2024-02-21 00:33:51 -0600

    commited on 21st Feb 2024

---
+++ Linked_List/ll_append.py
@@ -0,0 +1,9 @@
+def apppend(self, value):
+    new_node = Node(value)
+    if self.head is None:
+        self.head = new_node
+        self.tail = new_node
+    else:
+        self.tail.next = new_node
+        self.tail = new_node
+    self.length += 1        
