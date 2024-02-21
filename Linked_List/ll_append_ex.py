04600a03479097bc9d41777893d774cac72ac920
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2024-02-21 00:46:46 -0600

    commited on 21st Feb 2024

---
+++ Linked_List/ll_append_ex.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value) -> None:
+                new_node = Node(value)
+                self.head = new_node
+                self.tail = new_node
+                self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def make_empty(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = now_node
+        self.length += 1
+
+my_linked_list = LinkedList(10)
+my_linked_list.make_empty()
+
+my_linked_list.append(5)
+my_linked_list.append(10)
+
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length)
+
+print('Printing List:')
+my_linked_list.print_list()
