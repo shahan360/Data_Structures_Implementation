18c61099c73ce8981b0500364a6e98c4367644b8
shahan360 <sbsb4961@gmail.com>
2025-04-13 15:49:15 -0500

    committed on 13th April 2025

--- Data_Structures/Stacks_Queues/EXERCISE-Stack-Constructor-SK.py
+++ Data_Structures/Stacks_Queues/EXERCISE-Stack-Constructor-SK.py
@@ -1,18 +1,20 @@
 # class Node:
     ## WRITE NODE CONSTRUCTOR HERE ##
-    #                               #
-    #                               #
-    #                               #
-    #                               #
-    #################################
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
         
 # class Stack:
     ## WRITE STACK CONSTRUCTOR HERE ##
-    #                                #
-    #                                #
-    #                                #
-    #                                #
-    ##################################
+class Stack:
+    def __init__(self, value):
+        new_node = Node(value) # Create a new node with the given value
+        self.top = new_node # Top of the stack is the new node
+        #self.bottom = new_node # Not needed in stack implementation as we only need to keep track of the top node
+        #self.next = None # Not needed in stack implementation as we only need to keep track of the top node
+        self.height = 1 # Height of the stack is 1 when we add the first node
 
 
 
