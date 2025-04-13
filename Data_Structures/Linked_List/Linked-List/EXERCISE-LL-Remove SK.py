8401b12f525e4b03ba4e5184621a2e551f9c886e
shahan360 <sbsb4961@gmail.com>
2025-04-12 22:14:59 -0500

    committed on 12th April 2025

--- Data_Structures/Linked_List/Linked-List/EXERCISE-LL-Remove SK.py
+++ Data_Structures/Linked_List/Linked-List/EXERCISE-LL-Remove SK.py
@@ -96,12 +96,19 @@ class LinkedList:
         return True  
 
     ## WRITE REMOVE METHOD HERE ##
-    #                            #
-    #                            #
-    #                            #
-    #                            #
-    ##############################
-  
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp 
 
 
 
--- Data_Structures/Linked_List/Linked-List/EXERCISE-LL-Reverse SK.py
+++ Data_Structures/Linked_List/Linked-List/EXERCISE-LL-Reverse SK.py
@@ -110,12 +110,17 @@ class LinkedList:
         return temp
 
     ## WRITE REVERSE METHOD HERE ##
-    #                             #
-    #                             #
-    #                             #
-    #                             #
-    ###############################
-  
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
 
 
 
