25664d862a9c4ecf16c25368e782272781b1e319
shahan360 <sbsb4961@gmail.com>
2025-04-13 11:16:37 -0500

    committed on 13th April 2025

--- Data_Structures/Doubly_Linked_List/EXERCISE-DLL-Insert-SK.py
+++ Data_Structures/Doubly_Linked_List/EXERCISE-DLL-Insert-SK.py
@@ -91,11 +91,22 @@ class DoublyLinkedList:
         return False
     
     ## WRITE INSERT METHOD HERE ##
-    #                            #
-    #                            #
-    #                            #
-    #                            #
-    ##############################
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        before = self.get(index - 1)
+        after = before.next
+        before.next = new_node
+        new_node.prev = before
+        new_node.next = after
+        after.prev = new_node
+        self.length += 1
+        return True
 
   
 
