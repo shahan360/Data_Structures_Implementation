79f139a4095795ecde9441238c67769f69e6038c
shahan360 <sbsb4961@gmail.com>
2025-04-13 16:39:07 -0500

    committed on 13th April 2025

--- Data_Structures/Stacks_Queues/EXERCISE-Queue-Dequeue-SK.py
+++ Data_Structures/Stacks_Queues/EXERCISE-Queue-Dequeue-SK.py
@@ -29,13 +29,17 @@ class Queue:
         return True
 
     ## WRITE DEQUEUE METHOD HERE ##
-    #                             #
-    #                             #
-    #                             #
-    #                             #
-    ###############################
-
- 
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+        self.length -= 1
+        return temp
 
  
 my_queue = Queue(1)
