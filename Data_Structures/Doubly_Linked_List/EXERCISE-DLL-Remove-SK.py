10d1b354a77fa8dca5d8d7d3d7a17e4ba31cd823
shahan360 <sbsb4961@gmail.com>
2025-04-13 11:33:24 -0500

    committed on 13th April 2025

--- Data_Structures/Doubly_Linked_List/EXERCISE-DLL-Remove-SK.py
+++ Data_Structures/Doubly_Linked_List/EXERCISE-DLL-Remove-SK.py
@@ -111,12 +111,21 @@ class DoublyLinkedList:
         return True  
 
     ## WRITE REMOVE METHOD HERE ##
-    #                            #
-    #                            #
-    #                            #
-    #                            #
-    ##############################
-  
+    def remove(self, index):
+        if index < 0  or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        temp = self.get(index)
+        temp.next.prev = temp.prev
+        temp.prev.next = temp.next
+        temp.prev = None
+        temp.next = None
+        self.length -= 1
+        return temp
+
 
 
 
