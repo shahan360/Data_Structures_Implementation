371794155325659f3294381596d9a243504416a9
shahan360 <sbsb4961@gmail.com>
2025-04-13 08:52:17 -0500

    committed on 13th April 2025

--- Data_Structures/Doubly_Linked_List/EXERCISE-DLL-Constructor-SK.py
+++ Data_Structures/Doubly_Linked_List/EXERCISE-DLL-Constructor-SK.py
@@ -1,19 +1,24 @@
-# class Node:
+class Node:
     ## WRITE NODE CONSTRUCTOR HERE ##
-    #                               #
-    #                               #
-    #                               #
-    #                               #
-    #################################
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None 
+
         
-# class DoublyLinkedList:
+class DoublyLinkedList:
     ## WRITE DLL CONSTRUCTOR HERE ##
-    #                              #
-    #                              #
-    #                              #
-    #                              #
-    ################################
-  
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+    
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
 
 
 
