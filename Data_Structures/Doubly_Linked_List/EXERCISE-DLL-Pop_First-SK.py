de60fd5c875d2a4a1e3fb9dba81f4776cd88bad5
shahan360 <sbsb4961@gmail.com>
2025-04-13 10:21:08 -0500

    committed on 13th April 2025

--- Data_Structures/Doubly_Linked_List/EXERCISE-DLL-Pop_First-SK.py
+++ Data_Structures/Doubly_Linked_List/EXERCISE-DLL-Pop_First-SK.py
@@ -57,11 +57,19 @@ class DoublyLinkedList:
         return True
 
     ## WRITE POP_FIRST METHOD HERE ##
-    #                               #
-    #                               #
-    #                               #
-    #                               #
-    #################################
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp        
 
 
 
