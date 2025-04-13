ecb16d5181cf4f999311a5bbc16ab1ed0410e812
shahan360 <sbsb4961@gmail.com>
2025-04-13 16:32:47 -0500

    committed on 13th April 2025

--- Data_Structures/Stacks_Queues/EXERCISE-Queue-Enqueue-SK.py
+++ Data_Structures/Stacks_Queues/EXERCISE-Queue-Enqueue-SK.py
@@ -18,11 +18,16 @@ class Queue:
             temp = temp.next
         
     ## WRITE ENQUEUE METHOD HERE ##
-    #                             #
-    #                             #
-    #                             #
-    #                             #
-    ###############################
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+
         
 
 
