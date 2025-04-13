30769d76221e1e6f388e8db58e4e40eefa1b5459
shahan360 <sbsb4961@gmail.com>
2025-04-13 16:04:43 -0500

    committed on 13th April 2025

--- Data_Structures/Stacks_Queues/EXERCISE-Stack-Pop-SK.py
+++ Data_Structures/Stacks_Queues/EXERCISE-Stack-Pop-SK.py
@@ -27,11 +27,14 @@ class Stack:
         return True
 
     ### WRITE POP METHOD HERE ###
-    #                           #
-    #                           #
-    #                           #
-    #                           #
-    #############################
+    def pop(self):
+        if self.height == 0:
+            return None
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -= 1
+        return temp
     
 
     
--- Data_Structures/Stacks_Queues/EXERCISE-Stack-Push-SK.py
+++ Data_Structures/Stacks_Queues/EXERCISE-Stack-Push-SK.py
@@ -17,12 +17,14 @@ class Stack:
             temp = temp.next
 
     ### WRITE PUSH METHOD HERE ###
-    #                            #
-    #                            #
-    #                            #
-    #                            #
-    ##############################
- 
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next  = self.top
+            self.top = new_node    
+        self.height += 1
 
 
 
