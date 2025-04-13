d2d47bfe42ad6e50a0af6c8b29153845eec0b839
shahan360 <sbsb4961@gmail.com>
2025-04-13 16:24:19 -0500

    committed on 13th April 2025

--- Data_Structures/Stacks_Queues/EXERCISE-Queue-Constructor-SK.py
+++ Data_Structures/Stacks_Queues/EXERCISE-Queue-Constructor-SK.py
@@ -1,24 +1,24 @@
 # class Node:
     ## WRITE NODE CONSTRUCTOR HERE ##
-    #                               #
-    #                               #
-    #                               #
-    #                               #
-    #################################
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
         
-# class Queue:
-    ## WRITE QUEUE CONSTRUCTOR HERE ##
-    #                                #
-    #                                #
-    #                                #
-    #                                #
-    ##################################
+class Queue:
+    # WRITE QUEUE CONSTRUCTOR HERE ##
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last  = new_node
+        self.length = 1
+    #################################
 
-    # def print_queue(self):
-    #     temp = self.first
-    #     while temp is not None:
-    #         print(temp.value)
-    #         temp = temp.next
+    def print_queue(self):
+        temp = self.first
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
 
 
 
