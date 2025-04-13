c8ad955c49069e6285397f53d050e46f888d71fc
shahan360 <sbsb4961@gmail.com>
2025-04-13 10:36:30 -0500

    committed on 13th April 2025

--- Data_Structures/Doubly_Linked_List/EXERCISE-DLL-Get-SK.py
+++ Data_Structures/Doubly_Linked_List/EXERCISE-DLL-Get-SK.py
@@ -72,11 +72,18 @@ class DoublyLinkedList:
         return temp
 
     ### WRITE GET METHOD HERE ###
-    #                           #
-    #                           #
-    #                           #
-    #                           #
-    #############################
+    def get(self, index):
+        if index < 0  or index >= self.length:
+            return None
+        temp = self.head
+        if index < self.length // 2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp
 
 
 
